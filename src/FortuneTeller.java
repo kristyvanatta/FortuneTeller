abb772b21768fe9e4429632f34811f2e5d4e04b8
kristyvanatta <kristygm@aol.com>
2019-01-06 20:17:41 -0500

    commit for color problems

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -5,19 +5,18 @@ public class FortuneTeller {
 	public static void main(String[] args) {
 		Scanner input = new Scanner (System.in);
 				
-		System.out.println("Welcome, So you want to know your future, do you?!\nCome and let Odessa see your future!");
+		System.out.println("Welcome, So you want to know your future, do you?!\nCome and let Odessa see into your future!");
 		System.out.println("Now tell me your first name.");
-		String responseFirst = input.nextLine();
+		String first = input.nextLine();
 		
-		System.out.println("That is an interesting name! \nNow your last.");
-		String responseLast = input.nextLine();
+		System.out.println("What an interesting name! \nNow your last.");
+		String last = input.nextLine();
 		System.out.println("I'm starting to see someting!");
 		
-		System.out.println("Enter your age");
-		int responseAge = input.nextInt();
+		System.out.println("What is your age");
+		int age = input.nextInt();
 		int retirementYears;
-		boolean isEven = (responseAge % 2) == 0; 
-		
+		boolean isEven = (age % 2) == 0; 
 		if (isEven) {
 			retirementYears = 23;
 					}
@@ -25,19 +24,16 @@ public class FortuneTeller {
 			retirementYears = 46;
 		}
 		
-		//like "such wisdom at your age" or "oh you have a lot to learn yet"
+		
 		System.out.println("Enter your birth month as a number");
-		int responseBirth = input.nextInt();
+		int birthMonth = input.nextInt();
+		int bankBalance;
+		
+				
 		//Bank balance is determined by birth month, if user enters anything other than
 		// 1-12 their balance is $0.00
-		
-		System.out.println("Enter your favorite color using ROYGBIV? \nType \"help\" for color options");
-		String responseColor = input.nextLine();
-		
-		
-
-		
-					
+		System.out.println("Tell Odessa your favorite color");
+		String color = input.nextLine();
 		
 		//don't forget to enter the help loop for the color scheme
 		// set a condition for mode of transportation based on favorite color
@@ -45,25 +41,23 @@ public class FortuneTeller {
 		int responseSiblings = input.nextInt();
 		String vacationHome; 
 		if (responseSiblings == 1) {
-		vacationHome = "city1";
+		vacationHome = "Honolulu, HI";
 		}
 		else if (responseSiblings == 2) {
-		vacationHome = "city2";
+		vacationHome = "Miami, FL";
 		} 
 		else if (responseSiblings == 3) {
-		vacationHome = "city3";
+		vacationHome = "Las Vegas, NV";
 		}
 		else {
-		vacationHome = "city4";
+		vacationHome = "Juneau, AK";
 		}
-		
+				
 		//offer a quit option at input lines that will print out "Nobody likes a quitter..."
 		//before ending the program
 		
 		 
-		//System.out.println( *[First Name]* *[Last Name]* will retire in *[retirementYears]* with 
-		//*[bank balance]* in the bank, a vacation home in *[location]*,
-		//and travel by *[mode of transportation]*);
+		//System.out.println("first " + " last " + will retire in +" retirementYears " +  with +" $bank balance " + in the bank, a vacation home in + " vacationHome" + , and travel by + " transportation! ");
 		
 
 	}
